2fdad02b33baf73d4396ad5a96c31e3b5a8705f0
bestzanarak <sittichok_b@cmu.ac.th>
2023-12-14 22:40:39 +0700

    completed

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-  char rank;
-  cout << "Input your rank: ";
-  cin >> rank;
-  switch(rank){
-    case 'S': cout << "You have received Super Ultra Rare Unit!!!\n";
-    case 'A': cout << "You have received 5 gems.\n";
-    case 'B': cout << "You have received 1 gems.\n";
-    case 'C': cout << "You have received 2000 coins.\n";
-    case 'D': cout << "You have received very KAK items.\n";
-  }
-  return 0;
+char findGrade(float score) {
+    if (score > 90) {
+        return 'A';
+    } else if (score > 75 && score < 90 ) {
+        return 'B';
+    } else if (score > 60 && score < 75) {
+        return 'C';
+    } else if (score > 45 && score <= 60) {
+         return 'D';
+    } else {
+        return 'F';
+    }
 }
+
